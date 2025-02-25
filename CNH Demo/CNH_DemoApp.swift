4e30501d01460ffb214e393100eec4fea38028e5
CuSoft LLC <jmcusac@icloud.com>
2025-02-25 13:06:02 -0600

    Initial Commit

---
+++ CNH Demo/CNH_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CNH_DemoApp.swift
+//  CNH Demo
+//
+//  Created by Jason Cusack on 2/25/25.
+//
+
+import SwiftUI
+
+@main
+struct CNH_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CNH DemoTests/CNH_DemoTests.swift
@@ -0,0 +1,16 @@
+//
+//  CNH_DemoTests.swift
+//  CNH DemoTests
+//
+//  Created by Jason Cusack on 2/25/25.
+//
+
+import Testing
+
+struct CNH_DemoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
